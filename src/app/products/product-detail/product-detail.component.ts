7bfab60e8dec6940209b83efed64a7646324ab5c
ldex <laurent.duveau+git@gmail.com>
2021-01-29 12:18:51 -0500

    Day 2 Lunch time

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { ProductService } from 'src/app/services/product.service';
 import { Product } from '../product.interface';
 
 @Component({
@@ -10,9 +13,27 @@ export class ProductDetailComponent implements OnInit {
 
   @Input() product: Product;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private productService: ProductService
+  ) { }
 
   ngOnInit(): void {
+    let id = + this.activatedRoute.snapshot.params['id'];
+
+    if(id) {
+      this
+      .productService
+      .products$
+      .pipe(
+        map(products => products.find(product => product.id === id))
+      )
+      .subscribe(
+        result => this.product = result
+      )
+    }
+
+
   }
 
 }
--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError, map, startWith } from 'rxjs/operators';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from '../product.interface';
 
@@ -13,7 +15,9 @@ export class ProductListComponent implements OnInit {
   title: string = 'Products';
   //products: Product[];
   products$: Observable<Product[]>;
+  productsNumber$: Observable<number>;
   selectedProduct: Product;
+  errorMessage: string;
 
   // Pagination
   pageSize = 5;
@@ -37,14 +41,34 @@ export class ProductListComponent implements OnInit {
 
   onSelect(product: Product): void {
     this.selectedProduct = product;
+    this.router.navigateByUrl('/products/' + product.id);
   }
 
-  constructor(private productService: ProductService) {
+  constructor(
+    private productService: ProductService,
+    private router: Router) {
 
   }
 
   ngOnInit(): void {
-    this.products$ = this.productService.products$;
+    this.products$ = this
+                        .productService
+                        .products$
+                        .pipe(
+                          catchError(
+                            error => {
+                              this.errorMessage = error;
+                              return EMPTY;
+                            }
+                          )
+                        );
+
+    this.productsNumber$ = this
+                            .products$
+                            .pipe(
+                              map(products => products.length),
+                              startWith(0)
+                            );
 
     // this
     //   .productService
